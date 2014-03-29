920a5d1b57d0beea4045b32012ecd3d446197270
pygar <pygar@debianhp>
2014-03-29 16:36:03 +1100

    net: multi chan/server support

--- irc.c
+++ irc.c
@@ -15,68 +15,78 @@ void getchan(char join[], char *chan, int irc_sock);
 int printt(char read[512]); 
 char last[512]; 
 void* chkmsg(void *sock);
+
+typedef struct ircdata {
+        int irc_sock;
+        char chan[50];
+	char ip[100];
+
+}ircdata;
+ircdata irc;
+
 int main(int argc, char *argv[]){
 if (argc != 3) {
         printf("Specify host and port: ./a.out <hostname> <port>\n"); 
         return 1;
     }
-char chan[50]; char ip[100]; char message[512]; char authnick[15]; 
-char authuser[35]; int irc_sock; int ret_tprntmsg; int ret_tchkmsg; 
+
+char message[512]; char authnick[15]; 
+char authuser[35]; int ret_tprntmsg; int ret_tchkmsg; 
 struct sockaddr_in server;
 
-	if((irc_sock = socket(AF_INET,SOCK_STREAM,0)) <0){
+	if((irc.irc_sock = socket(AF_INET,SOCK_STREAM,0)) <0){
 	printf("Could not create socket\n");
 	}
 
-if ((hostip(argv[1],ip)) == 1) { return 0; };  
-server.sin_addr.s_addr = inet_addr(ip);
+if ((hostip(argv[1],irc.ip)) == 1) { return 0; };  
+server.sin_addr.s_addr = inet_addr(irc.ip);
 server.sin_family = AF_INET;
 server.sin_port = htons(atoi(argv[2]));
 
-	if(connect(irc_sock, (struct sockaddr *)&server, sizeof(server))){
+	if(connect(irc.irc_sock, (struct sockaddr *)&server, sizeof(server))){
 	printf("connect error\n");
 	}
 pthread_t tprntmsg; pthread_t tchkmsg;
-ret_tprntmsg = pthread_create(&tprntmsg,NULL,prntmsg,(void*)(intptr_t)irc_sock);
-ret_tchkmsg  = pthread_create(&tchkmsg,NULL,chkmsg,(void*)(intptr_t)irc_sock); 
+ret_tprntmsg = pthread_create(&tprntmsg,NULL,prntmsg,(void*)irc.irc_sock);
+ret_tchkmsg  = pthread_create(&tchkmsg,NULL,chkmsg,(void*)&irc); 
 snprintf(authnick, sizeof(authnick), "NICK %s\n\r", getenv("USER")); 
 snprintf(authuser, sizeof(authuser), "USER %s 8 * Jack U. Lemmon\n\r", getenv("USER")); 
-	sayraw(authnick,irc_sock);
-	sayraw(authuser,irc_sock);
+	sayraw(authnick,irc.irc_sock);
+	sayraw(authuser,irc.irc_sock);
 
 while(1){ 
 fgets(message, sizeof(message), stdin);
 
 	if(!(strncmp(message,"JOIN",4))){
-	sayraw(message, irc_sock);
-	getchan(message, chan, irc_sock);
+	sayraw(message, irc.irc_sock);
+	getchan(message, irc.chan, irc.irc_sock);
 	}	
 		else if(!(strncmp(message,"QUIT",4))){
-		sayraw(message, irc_sock);
+		sayraw(message, irc.irc_sock);
 		break; 
 		}
 			else if(!(strncmp(message, "PRIVATE", 7))){
-			getchan(message, chan, irc_sock); 
+			getchan(message, irc.chan, irc.irc_sock); 
 			}	
 				else if(!(strncmp(message, "LIST", 4))){
-				sayraw(message, irc_sock);
+				sayraw(message, irc.irc_sock);
 				}	
 					else if(!(strncmp(message, "WHOIS", 5))){
-					sayraw(message, irc_sock); 
+					sayraw(message, irc.irc_sock); 
 					}	
 						else if(!(strncmp(message, "NICK", 5))){
-						sayraw(message, irc_sock);
+						sayraw(message, irc.irc_sock);
 						}		
 							else{ 
-							mesg(message,chan,irc_sock);
+							mesg(message,irc.chan,irc.irc_sock);
 							}					
 }
-close(irc_sock);
+close(irc.irc_sock);
 return 0;
 }
 void* prntmsg(void *sock){
 char read[512];
-int irc_sock = (intptr_t)sock;
+int irc_sock = (int)sock;
 	while(1){
 	memset(read, 0, sizeof(read)); 
 	recv(irc_sock, read, sizeof(read), 0); //ALL data recv happens here
@@ -100,7 +110,7 @@ snprintf(privmsg, sizeof(privmsg), "PRIVMSG %s%s%s%s", chan, " :", write,"\r\n")
 sayraw(privmsg,irc_sock); 
 }
 
-void getchan(char join[], char *chan, int irc_sock){
+void getchan(char join[], char *chan, int irc_sock ){
 char *hash = (char *)malloc(50);
 hash = strchr(join, '#'); 
 snprintf(chan,strlen(hash),hash); 
@@ -131,18 +141,17 @@ printf("%c", read[x]);
 printf("\x1B[0m"); //normal
 char *msg = (char *)malloc(512);
 msg = strchr(read+1, ':');
-printf("%s",msg);
-snprintf(last,512,msg); 	
+strncpy(last, msg, sizeof(last)); 	
+printf("%s",last);
 return 0;
 }
 
-void* chkmsg(void* sock){
-int irc_sock = (intptr_t)sock;
+void* chkmsg(void  *ircs){
+struct ircdata *irc = (struct ircdata*)(ircs);
 	while(1){
 		if(!strncmp(last+1,"hi",2)){
-		mesg("sup","#sall",irc_sock);
+		mesg("sup",irc->chan,irc->irc_sock);
 		memset(last,'\0',512);
 		}
-// functions to run here
 	}
 }
